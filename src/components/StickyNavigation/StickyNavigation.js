677703a551a02a6f63c20eaa5e3100e3fa5a531d
Joseph Lynn <josephlynn94@gmail.com>
2020-07-08 09:19:48 -0400

    changed sticky navigation and main navigation to flexbox in place of grid

--- src/components/StickyNavigation/StickyNavigation.js
+++ src/components/StickyNavigation/StickyNavigation.js
@@ -6,36 +6,27 @@ import './styles.scss';
 function StickyNavigation() {
   return (
     <nav className='sticky-navigation'>
-      {/* Navigation List */}
-      <div className='navigation-list'>
-        {/* Navigation Items */}
-        <ul className='navigation-ul'>
-          <li className='navigation-item'>
-            <Link className='navigation-link' to='/'>
-              <div className='navigation-icon'>
-                <i className='fal fa-laptop-code'></i>
-                <p>Portfolio</p>
-              </div>
-            </Link>
-          </li>
-          <li className='navigation-item'>
-            <Link className='navigation-link' to='/'>
-              <div className='navigation-icon'>
-                <i className='fal fa-code'></i>
-                <p>Skills</p>
-              </div>
-            </Link>
-          </li>
-          <li className='navigation-item'>
-            <Link className='navigation-link' to='/'>
-              <div className='navigation-icon'>
-                <i className='fal fa-address-card'></i>
-                <p>Contact</p>
-              </div>
-            </Link>
-          </li>
-        </ul>
-      </div>
+      {/* Navigation ul */}
+      <ul className='navigation-ul'>
+        <li className='navigation-item'>
+          <Link className='navigation-link' to='#'>
+            <i className='fal fa-laptop-code navigation-icon'></i>
+            <span>Portfolio</span>
+          </Link>
+        </li>
+        <li className='navigation-item'>
+          <Link className='navigation-link' to='#'>
+            <i className='fal fa-code navigation-icon'></i>
+            <span>Skills</span>
+          </Link>
+        </li>
+        <li className='navigation-item'>
+          <Link className='navigation-link' to='#'>
+            <i className='fal fa-address-card navigation-icon'></i>
+            <span>Contact</span>
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
